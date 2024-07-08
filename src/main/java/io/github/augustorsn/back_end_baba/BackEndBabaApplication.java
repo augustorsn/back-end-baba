ff2be94da9c4df0e8fd184f99ae9919bddcc9eb0
Augusto Rodrigues De Santana Neto <51301642+augustorsn@users.noreply.github.com>
2024-07-08 20:12:40 -0300

    fix: add comentarios

--- src/main/java/io/github/augustorsn/back_end_baba/BackEndBabaApplication.java
+++ src/main/java/io/github/augustorsn/back_end_baba/BackEndBabaApplication.java
@@ -31,26 +31,26 @@ public class BackEndBabaApplication {
 	}
 
 
-	@Bean
-	public CommandLineRunner init(@Autowired ClientesJpa clientes, @Autowired PedidoJpa pedidos){
-		return args -> {			
-			System.out.println("Salvando Clientes");
-			Cliente fulano = new Cliente("Fulano");
-			clientes.save(fulano);
-			Pedido p = new Pedido();
-			p.setCliente(fulano);
-			p.setDataPedido(LocalDate.now());
-			p.setTotal(BigDecimal.valueOf(150));
-			pedidos.save(p);
-//			Cliente result = clientes.findClienteFetchPedidos(fulano.getId());
-//			System.out.println("Clientes"+ result);
-
-			pedidos.findByCliente(fulano).forEach(System.out::println);
-
-
-			
-		};
-	}
+//	@Bean
+//	public CommandLineRunner init(@Autowired ClientesJpa clientes, @Autowired PedidoJpa pedidos){
+//		return args -> {
+//			System.out.println("Salvando Clientes");
+//			Cliente fulano = new Cliente("Fulano");
+//			clientes.save(fulano);
+//			Pedido p = new Pedido();
+//			p.setCliente(fulano);
+//			p.setDataPedido(LocalDate.now());
+//			p.setTotal(BigDecimal.valueOf(150));
+//			pedidos.save(p);
+////			Cliente result = clientes.findClienteFetchPedidos(fulano.getId());
+////			System.out.println("Clientes"+ result);
+//
+//			pedidos.findByCliente(fulano).forEach(System.out::println);
+//			System.out.println("asdads");
+//
+//
+//		};
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndBabaApplication.class, args);
 	}
