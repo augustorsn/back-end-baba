738ca5cc9024f0a3e47ce953f9e07a9465f2c792
Augusto Rodrigues De Santana Neto <51301642+augustorsn@users.noreply.github.com>
2024-07-13 19:37:43 -0300

    fix: update rest

--- src/main/java/io/github/augustorsn/back_end_baba/rest/controller/ClienteController.java
+++ src/main/java/io/github/augustorsn/back_end_baba/rest/controller/ClienteController.java
@@ -14,13 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import io.github.augustorsn.back_end_baba.domain.Cliente;
 import io.github.augustorsn.back_end_baba.repository.ClientesJpa;
-
-
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -31,61 +30,70 @@ public class ClienteController {
     public ClienteController(ClientesJpa clientes) {
         this.clientes = clientes;
     }
-    
-    @GetMapping("{id}")    
+
+    @GetMapping("{id}")
     public Cliente getClienteById(@PathVariable Integer id) {
         Optional<Cliente> cliente = clientes.findById(id);
         if (cliente.isPresent()) {
             return cliente.get();
         }
-       throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "cliente não encontrado");
     }
 
-    @SuppressWarnings("rawtypes")
-    @PostMapping   
-    public ResponseEntity save(@RequestBody Cliente cliente) {
-        Cliente clienteSave = clientes.save(cliente);
-        return ResponseEntity.ok(clienteSave);
-    }
-
-    @SuppressWarnings("rawtypes")
-    @DeleteMapping("{id}")    
-    public ResponseEntity delete(@PathVariable Integer id) {
-        Optional<Cliente> c = clientes.findById(id);
-        if (c.isPresent()) {
-            clientes.delete(c.get());
-            return ResponseEntity.noContent().build();
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente save(@RequestBody Cliente cliente) {
+        try {
+            return clientes.save(cliente);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível salvar o cliente");
         }
-        return ResponseEntity.notFound().build();
+
     }
 
-    @SuppressWarnings("rawtypes")
-    @PutMapping("{id}")    
-    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente) {
-
-        Optional<Cliente> c = clientes.findById(id);
-
-        if (c.isPresent()) {
-            cliente.setId(c.get().getId());
-            clientes.save(cliente);
-            return ResponseEntity.noContent().build();
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        try {
+            Optional<Cliente> c = clientes.findById(id);
+            if (c.isPresent()) {
+                clientes.delete(c.get());
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível deletar o cliente");
         }
-        return ResponseEntity.notFound().build();
     }
 
-  
- 
-    
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id, @RequestBody Cliente cliente) {
+
+        try {
+            Optional<Cliente> c = clientes.findById(id);
+            if (c.isPresent()) {
+                cliente.setId(c.get().getId());
+                clientes.save(cliente);
+            }            
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível atualizar o cliente");
+        }
+    }
+
     @SuppressWarnings({ "rawtypes", "unchecked" })
-    @GetMapping   
-    public ResponseEntity find(Cliente filtro){
+    @GetMapping
+    public List<Cliente> find(Cliente filtro) {
+
+        try {
+            ExampleMatcher matcher = ExampleMatcher.matching()
+                    .withIgnoreCase()
+                    .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+            Example example = Example.of(filtro, matcher);
+            List<Cliente> lista = clientes.findAll(example);
+            return lista;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível listar o cliente");
+        }
         
-        ExampleMatcher matcher = ExampleMatcher.matching()
-                                                .withIgnoreCase()
-                                                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
-        Example example = Example.of(filtro,matcher);
-        List<Cliente> lista = clientes.findAll(example);
-        return ResponseEntity.ok(lista);        
     }
 
 }
