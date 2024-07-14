63610d1ccea94237180f49b372297be8b63f559f
Augusto Rodrigues De Santana Neto <51301642+augustorsn@users.noreply.github.com>
2024-07-14 16:55:40 -0300

    feat: creat produtos

--- src/main/java/io/github/augustorsn/back_end_baba/rest/controller/ProdutoController.java
+++ src/main/java/io/github/augustorsn/back_end_baba/rest/controller/ProdutoController.java
@@ -6,17 +6,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import io.github.augustorsn.back_end_baba.domain.Produto;
 import io.github.augustorsn.back_end_baba.repository.ProdutoJpa;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 
-      private ProdutoJpa produtos;
+    private ProdutoJpa produtos;
 
     public ProdutoController(ProdutoJpa produtos) {
         this.produtos = produtos;
@@ -30,5 +34,14 @@ public class ProdutoController {
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "produto não encontrado");
     }
-    
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto saveProduto(@RequestBody Produto produto) {
+        try {
+            return produtos.save(produto);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "produto não encontrado");
+        }
+    }
 }
