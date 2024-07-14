80dd133d3d9d83f6c23670882a23feb8b808c738
Augusto Rodrigues De Santana Neto <51301642+augustorsn@users.noreply.github.com>
2024-07-14 18:28:23 -0300

    fix: crud rest produto

--- src/main/java/io/github/augustorsn/back_end_baba/rest/controller/ProdutoController.java
+++ src/main/java/io/github/augustorsn/back_end_baba/rest/controller/ProdutoController.java
@@ -1,8 +1,12 @@
 package io.github.augustorsn.back_end_baba.rest.controller;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import io.github.augustorsn.back_end_baba.domain.Cliente;
 import io.github.augustorsn.back_end_baba.domain.Produto;
 import io.github.augustorsn.back_end_baba.repository.ProdutoJpa;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -44,4 +50,48 @@ public class ProdutoController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "produto não encontrado");
         }
     }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        try {
+            Optional<Produto> p = produtos.findById(id);
+            if (p.isPresent()) {
+                produtos.delete(p.get());
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível deletar o produto");
+        }
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id, @RequestBody Produto produto) {
+
+        try {
+            Optional<Produto> p = produtos.findById(id);
+            if (p.isPresent()) {
+                produto.setId(p.get().getId());
+                produtos.save(produto);
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível atualizar o produto");
+        }
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @GetMapping
+    public List<Produto> find(Produto filtro) {
+        try {
+            ExampleMatcher matcher = ExampleMatcher.matching()
+                    .withIgnoreCase()
+                    .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+            Example example = Example.of(filtro, matcher);
+            List<Produto> lista = produtos.findAll(example);
+            return lista;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível listar o produto");
+        }
+    }
+
 }
