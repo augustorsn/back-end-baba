3c4627c00d74cf7f7dc7d7b77f52b8dacdb76884
Augusto Rodrigues De Santana Neto <51301642+augustorsn@users.noreply.github.com>
2024-07-16 14:45:31 -0300

    feat: get pedido temp

--- src/main/java/io/github/augustorsn/back_end_baba/rest/controller/PedidoController.java
+++ src/main/java/io/github/augustorsn/back_end_baba/rest/controller/PedidoController.java
@@ -1,6 +1,8 @@
 package io.github.augustorsn.back_end_baba.rest.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,16 +10,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.augustorsn.back_end_baba.domain.Pedido;
+import io.github.augustorsn.back_end_baba.exception.RegraNegocioException;
+import io.github.augustorsn.back_end_baba.repository.PedidoJpa;
 import io.github.augustorsn.back_end_baba.rest.dto.PedidoDTO;
 import io.github.augustorsn.back_end_baba.service.impl.PedidoServiceImpl;
+
 
 @RestController
 @RequestMapping("/api/pedidos")
 public class PedidoController {
     private PedidoServiceImpl service;
+    private PedidoJpa pedidoRepository;
 
-    public PedidoController(PedidoServiceImpl service) {
+    public PedidoController(PedidoServiceImpl service, PedidoJpa pedidoRepository) {
         this.service = service;
+        this.pedidoRepository = pedidoRepository;
     }
 
     @PostMapping
@@ -28,5 +35,15 @@ public class PedidoController {
         return pedidoNew.getId();
 
     }
+
+    @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Pedido getPedido(@PathVariable Integer id) {
+        java.util.Optional<Pedido> pedido = pedidoRepository.findById(id);
+        if(pedido.isPresent()){
+            return pedido.get();
+        }
+        throw new RegraNegocioException("pedido com o código "+id+" não encontrado");       
+    } 
 
 }
